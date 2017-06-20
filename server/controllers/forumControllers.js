a92e3bbc6b90c117ef8cc6116ae78b1abb018847
Sean <sevensidedsilver@gmail.com>
2017-06-20 15:25:02 -0600

    moving to databases

---
+++ server/controllers/forumControllers.js
@@ -0,0 +1,44 @@
+var app = require('../server.js')
+
+// fetch all users
+module.exports = {
+  fetchUsers : function(req, res) {
+    req.app.get('db').fetchUsers().then(function(resp){
+      //console.log("neat")
+      res.status(200).send(resp)
+    })
+  },
+
+// fetch all threads
+  fetchThreads : function(req, res) {
+    req.app.get('db').fetchThreads().then(function(resp){
+      res.status(200).send(resp)
+    })
+  },
+
+  fetchoneThread : function(req, res) {
+    req.app.get('db').select_thread_by_id([req.params.id]).then(function(resp){
+      res.status(200).send(resp)
+    })
+  },
+
+
+//create new thread
+  new_thread : function(req, res) {
+    let thread_author = req.body.thread_author
+    let thread_title = req.body.thread_title
+    let thread_content = req.body.thread_content
+    req.app.get('db').new_Thread([thread_author, thread_title, thread_content]).then(function(resp){
+
+      res.status(200).send(resp)
+
+    })
+  }
+
+
+
+
+
+
+// end of module exports ====================
+}
--- server/server.js
+++ server/server.js
@@ -6,10 +6,28 @@ const session = require('express-session');
 const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
 const config = require('../config.js');
+const LOCALconfig = require('../LOCALconfig.js');
+
+var forumController = require('./controllers/forumControllers.js')
+
 
 // INITIATE EXPRESS APP & SET LISTENING PORT ================
 const app = express();
 const port = 3000;
+
+// MIDDLEWARE FOR EVERYTHING TO PASS THROUGH ================
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}./../public`));
+
+//express session =======================
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: config.sessionSecret
+}))
+
+
 
 // initialize auth0 =========================================
 app.use(passport.initialize())
@@ -19,8 +37,11 @@ passport.use(new Auth0Strategy({
   domain: config.auth0.domain,
   clientID: config.auth0.clientID,
   clientSecret: config.auth0.clientSecret,
-  callbackURL: '/auth/callback'
+  callbackURL: 'http://localhost:3000' + '/auth/callback'
 }, function(accessToken, refreshToken, extraParams, profile, done){
+  //console.log(profile)
+
+
   return done(null, profile);
 }))
 
@@ -30,15 +51,22 @@ passport.use(new Auth0Strategy({
 app.get('/auth', passport.authenticate('auth0'));
 
 app.get('/auth/callback',
-  passport.authenticate('auth0', {successRedirect: '/'}), function(req, res) {
+
+	// where to redirect after login
+  passport.authenticate('auth0', {successRedirect: '/#!/forum'}), function(req, res) {
     res.status(200).send(req.user);
 })
 
 
 
+
+
+
 // create deserialize/ serializer methods on passport
 passport.serializeUser(function(user, done) {
+
   done(null, user);
+
 });
 
 passport.deserializeUser(function(obj, done) {
@@ -47,9 +75,11 @@ passport.deserializeUser(function(obj, done) {
 
 
 app.get('/auth/me', function(req, res) {
-  if (!req.user) return res.sendStatus(404);
+  //console.log("TESTING", req.user)
+  if (!req.user) return res.status(200).send({user: false});
   //THIS IS WHATEVER VALUE WE GOT FROM userC variable above.
-  res.status(200).send(req.user);
+
+  res.status(200).send({user: req.user});
 })
 
 app.get('/auth/logout', function(req, res) {
@@ -61,25 +91,30 @@ app.get('/auth/logout', function(req, res) {
 
 
 
-// MIDDLEWARE FOR EVERYTHING TO PASS THROUGH ================
-app.use(bodyParser.json());
-app.use(express.static(`${__dirname}./../public`));
-app.use(cors());
+
 
 //// MASSIVE DB ==========================================
 massive({
-	host: 'localhost',
+	host: "localhost",
 	port: 5432,
-	database: 'forum',
-	user: '',
-	password: ''
+	database: LOCALconfig.database
+// user: config.dbuser,
+// password: config.dbpass
+
 }).then(db => {
 	app.set('db', db);
 });
 
+// FORUM endpoints ===========================================================
+app.get('/users', forumController.fetchUsers)
+
+app.get('/threads', forumController.fetchThreads)
+
+app.get('/thread/:id', forumController.fetchoneThread)
 
 
-
+// create new thread ========================POST POST POST
+app.post('/newthread', forumController.new_thread)
 
 
 // LISTENING ON PORT ===============================
